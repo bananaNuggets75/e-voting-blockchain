15d1c5c67a57865e9226aff0f057353f87259c98
Kenan Ben <gazokenan@gmail.com>
2025-07-20 00:59:58 +0800

    Add models: Voter, Candidate, Vote, Block

--- blockchain/evoting/models.py
+++ blockchain/evoting/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+import hashlib
+import json
+from datetime import datetime
 
-# Create your models here.
+class Voter(models.Model):
+    name = models.CharField(max_length=100)
+    has_voted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=100)
+    party = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} ({self.party})"
+
+class Vote(models.Model):
+    voter = models.OneToOneField(Voter, on_delete=models.CASCADE)
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.voter} voted for {self.candidate}"
+
+class Block(models.Model):
+    index = models.IntegerField()
+    vote_data = models.JSONField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    previous_hash = models.CharField(max_length=64)
+    hash = models.CharField(max_length=64)
+
+    def save(self, *args, **kwargs):
+        """Generate hash before saving"""
+        block_string = json.dumps({
+            "index": self.index,
+            "vote_data": self.vote_data,
+            "timestamp": self.timestamp.isoformat(),
+            "previous_hash": self.previous_hash,
+        }, sort_keys=True).encode()
+
+        self.hash = hashlib.sha256(block_string).hexdigest()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Block {self.index} - Hash: {self.hash[:10]}..."
