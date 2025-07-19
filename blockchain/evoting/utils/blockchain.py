d633bacc6a8d74b78c92ebd34e81580e6b9e5faf
Kenan Ben <gazokenan@gmail.com>
2025-07-20 01:54:14 +0800

    fix results

--- blockchain/evoting/utils/blockchain.py
+++ blockchain/evoting/utils/blockchain.py
@@ -1,13 +1,12 @@
 import hashlib
 import json
-from datetime import datetime
 
 def generate_hash(index, vote_data, timestamp, previous_hash):
     """Generate SHA-256 hash of block"""
     block_string = json.dumps({
         "index": index,
         "vote_data": vote_data,
-        "timestamp": timestamp.isoformat(),
+        "timestamp": timestamp,  
         "previous_hash": previous_hash
     }, sort_keys=True).encode()
 
